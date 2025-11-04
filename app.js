d1f383fc030a9315f1ab0d32f441e0228ebbf833
Sergio Bonaque Rubio <s.bonaquecontacto@gmail.com>
2025-11-04 13:56:25 +0000

    guardar seguimiento

---
+++ app.js
@@ -0,0 +1,20 @@
+// One euro is:
+let oneEuroIs = {
+    "JPY": 156.5, // japan yen
+    "USD": 1.07, // us dollar
+    "GBP": 0.87, // british pound
+}
+
+function fromDollarToYen (dollars){
+let euros = dollars / oneEuroIs["USD"];
+return euros * oneEuroIs ["JPY"];
+
+}
+
+function fromEuroToDollar = {
+
+}
+
+function fromYenToPound = {
+
+}
